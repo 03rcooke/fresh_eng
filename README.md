163d26be9c199bdf9507c46a2926505d108b3d0b
03rcooke <rob_cooke@hotmail.co.uk>
2023-03-22 11:36:09 +0000

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# fresh_eng
+<!-- badges: start -->
+[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/ACSLabUVic/diverse-predatory-niche/blob/main/LICENSE)
+<!-- badges: end -->
+
+# TITLE
+## fresh_eng repository
+
+This repository contains the required R code to reproduce the analyses and plots in: 'TITLE'. Rob Cooke, Ellie E. Dyer, Diana Bowler, Andrew C. Johnson, Mark Logie & Nick J.B. Isaac.
+
+***
+
+## Trend analyses
+
+See `fresh_eng.R` (Fig. 1; Fig. S1; Fig. S5; Fig. S6; Fig. S7)
+
+## Growth rate analyses and data availability plots
+
+See `msi_growth_rates.R` (Fig. 2; Fig. S2; Fig. S3; Fig. S4; Fig. S6; Fig. S7)
+
+## Example occupancy model
+
+`example_jags_model.txt` contains the JAGS code from an example occupancy model
--- msi_growth_rates.R
+++ msi_growth_rates.R
@@ -242,8 +242,76 @@ cowplot::save_plot("outputs/fig_2_msi_growth_rates.png", msi_growth_rates, base_
 
 # relate trends to data availability
 
+# get each visit dataset underlying each model
+
+# data owned by recording schemes
+
+# df_Trichoptera <- readRDS("/data-s3/occmods/Trichoptera/input_data/2021_Ellcur/visitData_caddisflies.rds")$occDetdata %>%
+#   tibble::add_column(Taxa = "Trichoptera")
+# 
+# df_Plecoptera <- readRDS("/data-s3/occmods/Plecoptera/input_data/2021_Ellcur/visitData_stoneflies.rds")$occDetdata %>%
+#   tibble::add_column(Taxa = "Plecoptera")
+# 
+# df_Ephemeroptera <- readRDS("/data-s3/occmods/Ephemeroptera/input_data/2021_Ellcur/visitData_mayflies.rds")$occDetdata %>%
+#   tibble::add_column(Taxa = "Ephemeroptera")
+
+# combine all
+all_df <- dplyr::bind_rows(df_Trichoptera,
+                           df_Plecoptera,
+                           df_Ephemeroptera)
+
+# map sites to basins
+catchmentData <- read.csv("data/districts_ALL_FINAL.csv") %>%
+  dplyr::rename(site = "SQ1_SQUARE",
+                catchment = "rbd_name")
+
+# for plots of data availability
+sum_df <- all_df %>%
+  dplyr::inner_join(., catchmentData, by="site") %>%
+  dplyr::filter(TP > 1969 & TP <2021) %>%
+  dplyr::filter(catchment != "Dee") %>%
+  dplyr::group_by(TP, catchment) %>%
+  dplyr::mutate(SY = paste(site,TP)) %>%
+  dplyr::summarise(nuSY = length(unique(SY)),
+                   nuVisits = length(unique(visit))) %>% 
+  dplyr::mutate(catchment = factor(catchment, levels = c("Solway Tweed", "Northumbria", "North West", "Humber", "Anglian", "Severn", "Thames", "South West", "South East")))
+
+set.seed(123)
+tol_catch <- sample(c('#77AADD', '#EE8866', '#EEDD88', '#FFAABB', '#99DDFF', '#44BB99', '#BBCC33', '#AAAA00', '#DDDDDD'))
+tol <- c(tol_catch[[2]], tol_catch[[3]], tol_catch[[1]], tol_catch[[4]], tol_catch[[5]], tol_catch[[9]], tol_catch[[6]], tol_catch[[8]], tol_catch[[7]])
+
+g1 <- ggplot2::ggplot(sum_df, ggplot2::aes(x = TP, y = nuVisits, group = catchment, colour = catchment)) +
+  ggplot2::geom_point() +
+  ggplot2::geom_line() +
+  ggplot2::scale_y_log10() +
+  ggplot2::scale_colour_manual(values = tol) +
+  ggplot2::labs(x = "Year", y = "Number of visits", colour = "") +
+  ggplot2::theme_bw()
+
+g2 <- ggplot2::ggplot(sum_df, ggplot2::aes(x = TP, y = nuSY, group = catchment, colour = catchment)) +
+  ggplot2::geom_point() +
+  ggplot2::geom_line() +
+  ggplot2::scale_y_log10() +
+  ggplot2::scale_colour_manual(values = tol) +
+  ggplot2::labs(x = "Year", y = "Number of site-years", colour = "") +
+  ggplot2::theme_bw()
+
+cowplot::plot_grid(g1, g2, nrow = 2)
+
+ggplot2::ggsave("outputs/fig_s2_dataAvail_ts.png", width = 6, height = 6)
+
+# for models to check relationship model predictions
+period_df <- all_df %>%
+  dplyr::inner_join(., catchmentData, by = "site") %>%
+  dplyr::filter(TP > 1969 & TP <2021) %>%
+  dplyr::mutate(SY = paste(site, TP),
+                period = ifelse(TP %in% 1970:1992, "First (1971-1992)", "Second (1993-2020)")) %>%
+  dplyr::group_by(period, catchment) %>%
+  dplyr::summarise(nuSY = length(unique(SY)),
+                   nuVisits = length(unique(visit))) 
+
 # see data_availability_ept.R
-dataAvailability <- readRDS("data/period_df.rds") %>%
+dataAvailability <- period_df %>%
   dplyr::mutate(agg = catchment)
 
 # add onto trend data
